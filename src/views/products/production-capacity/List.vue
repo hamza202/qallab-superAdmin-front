33da55019b3581d2936507aea033f8428c271438
Hamza Abo Aitah <26406612+hamza202@users.noreply.github.com>
2026-01-23 19:29:20 +0200

    Refactor List.vue to comment out unused computed properties and bulk delete functionality, and streamline the DataTable component by removing unnecessary actions.

--- src/views/products/production-capacity/List.vue
+++ src/views/products/production-capacity/List.vue
@@ -77,7 +77,7 @@ const hasMoreData = computed(() => nextCursor.value !== null)
 
 // Selection state
 const selectedRows = ref<number[]>([])
-const hasSelected = computed(() => selectedRows.value.length > 0)
+// const hasSelected = computed(() => selectedRows.value.length > 0)
 
 // Status change dialog
 const showStatusChangeDialog = ref(false)
@@ -110,18 +110,6 @@ const productionCapacityIcon = `<svg width="52" height="52" viewBox="0 0 52 52" 
 <path fill-rule="evenodd" clip-rule="evenodd" d="M13.0002 25.9998C9.41035 25.9998 6.5002 28.9099 6.5002 32.4998V38.9998C6.5002 42.5896 9.41035 45.4998 13.0002 45.4998C16.59 45.4998 19.5002 42.5896 19.5002 38.9998V32.4998C19.5002 28.9099 16.5901 25.9998 13.0002 25.9998ZM10.8335 32.4998C10.8335 31.3032 11.8036 30.3331 13.0002 30.3331C14.1968 30.3331 15.1669 31.3032 15.1669 32.4998V38.9998C15.1669 40.1964 14.1968 41.1664 13.0002 41.1664C11.8036 41.1664 10.8335 40.1964 10.8335 38.9998V32.4998Z" fill="#1570EF"/>
 </svg>
 `
-
-const editIcon = `<svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M8.33301 2.60175H4.83301C3.43288 2.60175 2.73281 2.60175 2.19803 2.87424C1.72763 3.11392 1.34517 3.49637 1.10549 3.96678C0.833008 4.50156 0.833008 5.20162 0.833008 6.60175V13.6018C0.833008 15.0019 0.833008 15.7019 1.10549 16.2367C1.34517 16.7071 1.72763 17.0896 2.19803 17.3293C2.73281 17.6018 3.43288 17.6018 4.83301 17.6018H11.833C13.2331 17.6018 13.9332 17.6018 14.468 17.3293C14.9384 17.0896 15.3208 16.7071 15.5605 16.2367C15.833 15.7019 15.833 15.0019 15.833 13.6018V10.1018M5.83299 12.6018H7.22844C7.63609 12.6018 7.83992 12.6018 8.03173 12.5557C8.20179 12.5149 8.36436 12.4475 8.51348 12.3562C8.68168 12.2531 8.8258 12.109 9.11406 11.8207L17.083 3.85175C17.7734 3.1614 17.7734 2.04211 17.083 1.35175C16.3927 0.661396 15.2734 0.661395 14.583 1.35175L6.61404 9.3207C6.32578 9.60896 6.18166 9.75308 6.07859 9.92128C5.9872 10.0704 5.91986 10.233 5.87904 10.403C5.83299 10.5948 5.83299 10.7987 5.83299 11.2063V12.6018Z" stroke="#175CD3" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>`
-
-const trash_1_icon = `<svg width="17" height="19" viewBox="0 0 17 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M11.5833 4.08333V3.41667C11.5833 2.48325 11.5833 2.01654 11.4017 1.66002C11.2419 1.34641 10.9869 1.09144 10.6733 0.931656C10.3168 0.75 9.85009 0.75 8.91667 0.75H7.58333C6.64991 0.75 6.1832 0.75 5.82668 0.931656C5.51308 1.09144 5.25811 1.34641 5.09832 1.66002C4.91667 2.01654 4.91667 2.48325 4.91667 3.41667V4.08333M0.75 4.08333H15.75M14.0833 4.08333V13.4167C14.0833 14.8168 14.0833 15.5169 13.8108 16.0516C13.5712 16.522 13.1887 16.9045 12.7183 17.1442C12.1835 17.4167 11.4835 17.4167 10.0833 17.4167H6.41667C5.01654 17.4167 4.31647 17.4167 3.78169 17.1442C3.31129 16.9045 2.92883 16.522 2.68915 16.0516C2.41667 15.5169 2.41667 14.8168 2.41667 13.4167V4.08333" stroke="#D92D20" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>`
-
-const trash_2_icon = `<svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M5.75 0.75H10.75M0.75 3.25H15.75M14.0833 3.25L13.4989 12.0161C13.4112 13.3313 13.3674 13.9889 13.0833 14.4875C12.8333 14.9265 12.456 15.2794 12.0014 15.4997C11.485 15.75 10.8259 15.75 9.50779 15.75H6.99221C5.67409 15.75 5.01503 15.75 4.49861 15.4997C4.04396 15.2794 3.66674 14.9265 3.41665 14.4875C3.13259 13.9889 3.08875 13.3313 3.00107 12.0161L2.41667 3.25M6.58333 7V11.1667M9.91667 7V11.1667" stroke="#D92D20" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>`
 
 const searchIcon = `<svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M15.8333 15.8335L12.9167 12.9168M14.9999 7.91683C14.9999 11.8288 11.8286 15.0002 7.91659 15.0002C4.00457 15.0002 0.833252 11.8288 0.833252 7.91683C0.833252 4.00481 4.00457 0.833496 7.91659 0.833496C11.8286 0.833496 14.9999 4.00481 14.9999 7.91683Z" stroke="white" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
@@ -239,12 +227,12 @@ const handleDelete = (item: any) => {
   showDeleteDialog.value = true
 }
 
-const handleBulkDelete = () => {
-  if (selectedRows.value.length === 0) return
-  deleteMode.value = 'bulk'
-  itemToDelete.value = null
-  showDeleteDialog.value = true
-}
+// const handleBulkDelete = () => {
+//   if (selectedRows.value.length === 0) return
+//   deleteMode.value = 'bulk'
+//   itemToDelete.value = null
+//   showDeleteDialog.value = true
+// }
 
 const confirmDelete = async () => {
   try {
@@ -389,19 +377,7 @@ onBeforeUnmount(() => {
       </div>
 
       <div class="bg-gray-50 rounded-md -mx-6">
-        <div :class="hasSelected ? 'justify-between' : 'justify-end'"
-          class="flex flex-wrap items-center gap-3 border-y border-y-slate-300 px-4 sm:px-6 py-3">
-          <!-- Actions when rows are selected -->
-          <div v-if="hasSelected"
-            class="flex flex-wrap items-stretch rounded-lg overflow-hidden border border-gray-200 bg-white text-sm">
-            <ButtonWithIcon variant="flat" height="40" rounded="0"
-              custom-class="px-4 font-semibold text-error-600 hover:bg-error-50/40 !rounded-none"
-              :prepend-icon="trash_1_icon" color="white" label="حذف المحدد" @click="handleBulkDelete" />
-            <div class="w-px bg-gray-200"></div>
-            <ButtonWithIcon variant="flat" height="40" rounded="0"
-              custom-class="px-4 font-semibold text-error-600 hover:bg-error-50/40 !rounded-none"
-              :prepend-icon="trash_2_icon" color="white" label="حذف الجميع" @click="handleBulkDelete" />
-          </div>
+        <div class="flex flex-wrap items-center gap-3 justify-end border-y border-y-slate-300 px-4 sm:px-6 py-3">
           <!-- Main header controls -->
           <div class="flex flex-wrap gap-3">
             <!-- Column Management -->
@@ -465,8 +441,8 @@ onBeforeUnmount(() => {
         </v-alert>
 
         <!-- Production Capacity Table -->
-        <DataTable :show-view="false" :headers="tableHeaders" :items="tableItems" :loading="isLoading" show-checkbox show-actions
-          @edit="handleEdit" @delete="handleDelete" @select="handleSelect" @selectAll="handleSelectAll">
+        <DataTable :show-view="false" :headers="tableHeaders" :items="tableItems" :loading="isLoading" show-actions
+          @edit="handleEdit" @delete="handleDelete">
           <template #item.is_active="{ item }">
             <v-switch :model-value="item.is_active" hide-details inset density="compact" color="primary"
               class="small-switch" @update:model-value="() => handleStatusChange(item)" />
